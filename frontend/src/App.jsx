a97b191b2144899c82d2baa896bedba4b75a006f
maitreya-16 <maitreyavaidyam@gmail.com>
2026-01-24 15:00:57 +0530

    admin-panel

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import AdminPanel from './components/admin'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <AdminPanel/>
+    </>
+  )
+}
+
+export default App
